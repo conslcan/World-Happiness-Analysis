d2973e1545b9cdf4e2f48dc621bced3f1642e9be
conslcan <conslcan@gmail.com>
2025-03-07 11:10:13 -0500

    updated the python main file

--- WHR.py
+++ WHR.py
@@ -1,9 +1,9 @@
 import pandas as pd  
 import matplotlib.pyplot as plt  
 import seaborn as sns  
-import sklearn 
-from sklearn.metrics import mean_absolute_error, r2_score
-
+from sklearn.model_selection import train_test_split  
+from sklearn.linear_model import LinearRegression  
+from sklearn.metrics import mean_squared_error 
 # Load the dataset
 df = pd.read_csv("data/world_happiness.csv")  
 
@@ -25,9 +25,7 @@ sns.heatmap(df_numeric.corr(), annot=True, cmap='coolwarm')
 plt.title("Correlation Between Happiness and Other Factors")
 plt.savefig('images/WHPlot.png', format='png')
 
-from sklearn.model_selection import train_test_split  
-from sklearn.linear_model import LinearRegression  
-from sklearn.metrics import mean_squared_error  
+ 
 X = df[['GDP per capita', 'Social support', 'Healthy life expectancy']]  
 y = df['Score']  
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  
